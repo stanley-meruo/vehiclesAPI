0ddacc9445eaca4e6debb7e6756b04cb9075145b
Stanley Meruo <144041200+stanley-meruo@users.noreply.github.com>
2025-05-23 12:23:29 +0100

    Enable CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const vehicleRoutes = require("./routes/vehicleRoutes");
 const dotenv = require('dotenv');
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
@@ -9,12 +10,18 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to our Shipping Vehicle API!");
 });
+
 // Middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // during development
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/vehicles", vehicleRoutes);
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
